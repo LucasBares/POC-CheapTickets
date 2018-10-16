c90531363f0153bfb66deea441f77abb101c634d
Lucas Bares <lucasbaresm@gmail.com>
2018-10-16 19:47:51 -0300

    Initial Commit

---
+++ CheapTickets/CheapTickets/Tests/Exercise1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using CheapTickets.Utils;
+using NUnit.Framework;
+
+namespace CheapTickets.Tests
+{
+    class Exercise1
+    {
+        [SetUp]
+        public void Inicializate()
+        {
+            Collection.driver = new ChromeDriver(@"C:\Users\lucas.mazzini\source\repos\ConsoleApp1\ConsoleApp1");
+        }
+
+        [Test]
+        public void OpenPage()
+        {
+            Collection.driver.Url = "https://www.cheaptickets.com/";
+
+            HeaderObjects headerObjects = new HeaderObjects();
+
+            headerObjects.Header.Click();
+        }
+    }
+}
---
+++ CheapTickets/CheapTickets/Utils/HeaderObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace CheapTickets.Utils
+{
+    class HeaderObjects
+    {
+        public HeaderObjects()
+        {
+            PageFactory.InitElements(Collection.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "primary-header-hotel")]
+        public IWebElement Header { get; set; }
+    }
+}
